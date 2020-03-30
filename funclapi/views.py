2c4e1311443684d714c3f99e1c94968566c0348a
YunJou <sheryltu880121@gmail.com>
2020-03-30 21:49:39 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -4,9 +4,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
-from linebot.models import MessageEvent, TextMessage
-from module import func
 from linebot.models import *
+from module import func
+
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECERT)
@@ -33,12 +33,9 @@ def callback(request):
 					#line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 					mtext = event.message.text
 					if mtext == '140':
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='嗨'))
-
-						
-						#func.sendText(event1)
-		#			elif mtext == '好':
-		#				func.sendQuickreply(event)
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='你的基礎代謝率為1024，請回傳好。'))
+					if mtext == '好':
+						func.sendQuickreply(event)
 					
 		return HttpResponse()
 	else:
--- module/func.py
+++ module/func.py
@@ -1,43 +1,45 @@
 from django.conf import settings
 
 from linebot import LineBotApi
-from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction
+from linebot.models import *
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
-def sendText(event1):
-	try:
-		message = TextSendMessage(
-			text = "你的基礎代謝率為1024，請回傳好。"
-		)
-		line_bot_api.reply_message(event1.reply_token,message)
-	except:
-		line_bot_api.reply_message(event1.reply_token,TextSendMessage(text='發生錯誤!'))
+#def sendText(event1):
+	#try:
+	#	message = TextSendMessage(
+	#		text = "你的基礎代謝率為1024，請回傳好。"
+	#	)
+	#	line_bot_api.reply_message(event1.reply_token,message)
+	#except:
+	#	line_bot_api.reply_message(event1.reply_token,TextSendMessage(text='發生錯誤!'))
 def sendQuickreply(event):
 	try:
 		message = TextSendMessage(
 			text='請選擇您的運動頻率',
-		quick_reply=QuickReply(
-			items=[
-				QuickReplyButton(
-				action=MessageAction(label='久坐',text='久坐')
-			),
-				QuickReplyButton(
-				action=MessageAction(label='除了通勤之外，不運動',text='除了通勤之外，不做其他運動')
-			),  
-				QuickReplyButton(
-				action=MessageAction(label='每周有一兩天',text='每周有一兩天會做運動')
-			), 
-				QuickReplyButton(
-				action=MessageAction(label='每周有三四天',text='每周有三四天會做運動')
-			), 
-				QuickReplyButton(
-				action=MessageAction(label='每周有五六天',text='每周有五六天會做運動')
-			), 
-				QuickReplyButton(
-				action=MessageAction(label='天天都運動',text='天天都會做運動')
-				),
-			]))
+			quick_reply=QuickReply(
+				items=[
+					QuickReplyButton(
+						action=MessageAction(label='久坐',text='久坐')
+					),
+					QuickReplyButton(
+						action=MessageAction(label='除了通勤之外，不運動',text='除了通勤之外，不做其他運動')
+					),  
+					QuickReplyButton(
+						action=MessageAction(label='每周有一兩天',text='每周有一兩天會做運動')
+					), 
+					QuickReplyButton(
+						action=MessageAction(label='每周有三四天',text='每周有三四天會做運動')
+					), 
+					QuickReplyButton(
+						action=MessageAction(label='每周有五六天',text='每周有五六天會做運動')
+					), 
+					QuickReplyButton(
+						action=MessageAction(label='天天都運動',text='天天都會做運動')
+					),
+				]
+			)
+		)
 		line_bot_api.reply_message(event.reply_token,message)
 	except:
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!'))
