1d03bbd52ea64edcc097fdbbf6348787d1e5c16f
YunJou <sheryltu880121@gmail.com>
2020-03-30 15:28:27 +0800

    03290604561

--- funclapi/views.py
+++ funclapi/views.py
@@ -33,13 +33,9 @@ def callback(request):
 					line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 					mtext = event.message.text
 					if mtext == '140':
-						try:
-							message = TextSendMessage(
-								text = "你的基礎代謝率為1024，請回傳好。"
-							)
-							line_bot_api.reply_message(event.reply_token,message)
-						except:
-							line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!'))
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='嗨'))
+
+						
 						#func.sendText(event1)
 		#			elif mtext == '好':
 		#				func.sendQuickreply(event)
