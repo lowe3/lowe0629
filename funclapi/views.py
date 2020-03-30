7e77c76e862225b526a35a2836ad7cd2df223c01
YunJou <sheryltu880121@gmail.com>
2020-03-30 14:47:59 +0800

    03290604561

--- funclapi/views.py
+++ funclapi/views.py
@@ -6,6 +6,7 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextMessage
 from module import func
+from linebot.models import *
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECERT)
@@ -24,16 +25,19 @@ def callback(request):
 		except LineBotApiError:
 			return HttpResponseBadRequest()
 
+		if event.message.text == "文字":
+			line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+			
 		#line_bot_api.reply_message("1")
-		for event in events:
-			if isinstance(event, MessageEvent):
-				if isinstance(event.message, TextMessage):
-					
-					mtext = event.message.text
-					if mtext == '女/140/40/18':
-						func.sendText(event1)
-					elif mtext == '好':
-						func.sendQuickreply(event)
+		#for event in events:
+		#	if isinstance(event, MessageEvent):
+		#		if isinstance(event.message, TextMessage):
+		#			
+		#			mtext = event.message.text
+		#			if mtext == '女/140/40/18':
+		#				func.sendText(event1)
+		#			elif mtext == '好':
+		#				func.sendQuickreply(event)
 					
 		return HttpResponse()
 	else:
