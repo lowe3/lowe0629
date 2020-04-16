f44604f5a6aaa2d60b54f478269713ae4a07b61a
YunJou <sheryltu880121@gmail.com>
2020-04-16 14:39:51 +0800

    03290601

--- LinebotFunc/settings.py
+++ LinebotFunc/settings.py
@@ -129,5 +129,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
       os.path.join(BASE_DIR,'static'),]
+	  
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600,ssl_require=True)
