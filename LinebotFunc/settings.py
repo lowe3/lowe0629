302e10b87cffecf8efe69fffaec25869714ce6d5
YunJou <sheryltu880121@gmail.com>
2020-04-16 14:45:43 +0800

    03290601

--- LinebotFunc/settings.py
+++ LinebotFunc/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import psycopg2
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -86,9 +85,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL,sslmodde='require')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
