32c152e2b534f06a7bd1b5bf403e02841ef39e7f
YunJou <sheryltu880121@gmail.com>
2020-05-18 00:04:18 +0800

    05161256

--- funclapi/views.py
+++ funclapi/views.py
@@ -35,19 +35,19 @@ def callback(request):
 				if isinstance(event.message, TextMessage):
 					line_id = event.source.user_id
 					mtext = event.message.text
-					if mtext[3:] == '公分':
-						height = ''.join([x for x in mtext if x.isdigit()])
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的身高是'+height+'公分，請輸入您的體重，EX:50公斤'))
-					elif mtext[-2:] == '公斤':
-						weight = ''.join([x for x in mtext if x.isdigit()])
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的體重是'+weight+'公斤，請輸入您的性別，EX：男性'))
+					if mtext[3:] == 'cm':
+						he = ''.join([x for x in mtext if x.isdigit()])
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的身高是'+he+'公分，請輸入您的體重，EX:50公斤'))
+					elif mtext[-2:] == 'kg':
+						we = ''.join([x for x in mtext if x.isdigit()])
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的體重是'+we+'公斤，請輸入您的性別，EX：男性'))
 					elif mtext == '男性':
 						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的先生，接下來請輸入您的年齡，EX：18歲'))
 					elif mtext == '女性':
 						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的小姐，接下來請輸入您的年齡，EX：18歲'))
 					elif mtext[2] == '歲':
-						years = ''.join([x for x in mtext if x.isdigit()])
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的年齡是'+years+'歲，感謝您的告知，接下請選擇您的運動頻率好嗎?'))
+						ye = ''.join([x for x in mtext if x.isdigit()])
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的年齡是'+ye+'歲，感謝您的告知，接下請選擇您的運動頻率好嗎?'))
 					elif mtext == '好':
 						func.sendQuickreply(event)
 					elif mtext == '推薦菜單':
