81dbf933c2c9c4f2a749dada600eaa318fb30028
YunJou <sheryltu880121@gmail.com>
2020-05-28 15:24:30 +0800

    05280938

--- funclapi/views.py
+++ funclapi/views.py
@@ -57,11 +57,11 @@ def callback(request):
 					elif mtext == '男性':
 						mb = 66+(13.7*we)+(5*he)-(6.8*ye)
 						mmb = '%d'%mb
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的先生'+mmb+'，您的基礎代謝率為，接下來請輸入您的運動頻率好嗎?'))
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的先生，您的基礎代謝率為'+mmb+'，接下來請輸入您的運動頻率好嗎?'))
 					elif mtext == '女性':
 						wb = 655+(9.6*we)+(1.8*he)-(4.7*ye)
 						wwb = '%d'%wb
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的先生'+wwb+'，您的基礎代謝率為，接下來請輸入您的運動頻率好嗎?'))	
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的小姐，您的基礎代謝率為'+hhe+'，接下來請輸入您的運動頻率好嗎?'))	
 					elif mtext == '推薦菜單':
 						func.sendImage(event)						
 					elif mtext == '@久坐':
