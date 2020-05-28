d7836d4aa29da1105adf68007136d773789cbafb
YunJou <sheryltu880121@gmail.com>
2020-05-28 13:59:59 +0800

    05280938

--- funclapi/views.py
+++ funclapi/views.py
@@ -52,8 +52,8 @@ def callback(request):
 						ye = ''.join([x for x in mtext if x.isdigit()])
 						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的年齡是'+ye+'歲，請輸入您的性別，EX：男性'))
 					elif mtext == '男性':
-						mb = 66+(13*we)+(5*he)-(6*ye)
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的先生，您的基礎代謝率為'+mb+'，接下來請輸入您的運動頻率好嗎?'))
+						#mb = 66+(13.7*we)+(5*he)-(6.8*ye)
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的先生，您的基礎代謝率為'+he+'，接下來請輸入您的運動頻率好嗎?'))
 					elif mtext == '女性':
 						wb = 655+(9.6*we)+(1.8*he)-(4.7*ye)
 						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的小姐，您的基礎代謝率為'+wb+'，接下來請輸入您的運動頻率好嗎?'))
