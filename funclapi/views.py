6fe9b620aa14f9ac7119a35cf81aa5b2ec0a139d
YunJou <sheryltu880121@gmail.com>
2020-05-28 15:13:20 +0800

    05280938

--- funclapi/views.py
+++ funclapi/views.py
@@ -44,7 +44,7 @@ def callback(request):
 						func.sendQuickreply(event)
 					elif mtext[-2:] == '公分':
 						he = int(x for x in mtext if x.isdigit())
-						hhe = '%d' %he
+						hhe = '%d'%he
 						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的身高是'+hhe+'公分，請輸入您的體重，EX:50公斤'))
 					elif mtext[-2:] == '公斤':
 						we = [x for x in mtext if x.isdigit()]
