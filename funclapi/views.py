318ffdb399c602ed03231ccbc5e93123c78ef5e6
YunJou <sheryltu880121@gmail.com>
2020-05-28 09:52:17 +0800

    05280938

--- funclapi/views.py
+++ funclapi/views.py
@@ -47,9 +47,9 @@ def callback(request):
 						ye = ''.join([x for x in mtext if x.isdigit()])
 						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的年齡是'+ye+'歲，接下來請輸入您的性別，EX：男性'))
 					elif mtext == '男性':
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的先生，接下請選擇您的運動頻率好嗎?'))
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的先生，接下來請選擇您的運動頻率好嗎?'))
 					elif mtext == '女性':
-						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的小姐，接下請選擇您的運動頻率好嗎?'))
+						line_bot_api.reply_message(event.reply_token, TextSendMessage(text='好的小姐，接下來請選擇您的運動頻率好嗎?'))
 					elif mtext == '推薦菜單':
 						func.sendImage(event)						
 					elif mtext == '@久坐':
