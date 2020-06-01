c9916aa53e6d6b418936e9d6b4eb73986ad4ad19
YunJou <sheryltu880121@gmail.com>
2020-06-01 14:40:09 +0800

    06010236

--- LinebotFunc/settings.py
+++ LinebotFunc/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
 import dj_database_url
+
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
