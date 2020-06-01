c4d0a41008b705706945180510ba0349fc735c0f
YunJou <sheryltu880121@gmail.com>
2020-06-01 14:47:14 +0800

    06010236

--- LinebotFunc/settings.py
+++ LinebotFunc/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
+import os
+import psycopg2
 import dj_database_url
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
