c56a4cc7e77861a5ec2bf75d44341eef3044de9b
YunJou <sheryltu880121@gmail.com>
2020-06-01 19:03:05 +0800

    06011850

--- LinebotFunc/settings.py
+++ LinebotFunc/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
 import psycopg2
-import dj_database_url
+#import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
