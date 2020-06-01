c5aaecfc2b48e209208e68a7f5f482359757d04c
YunJou <sheryltu880121@gmail.com>
2020-06-01 14:49:17 +0800

    06010236

--- funclapi/views.py
+++ funclapi/views.py
@@ -11,9 +11,6 @@ import random
 import os
 import psycopg2
 
-#DATABASE_URL = os.environ['DATABASE_URL']
-#conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECERT)
