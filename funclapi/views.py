c0e075497455ad9b36d79b1198411fb1091014f2
YunJou <sheryltu880121@gmail.com>
2020-06-22 12:51:20 +0800

    062110013

--- funclapi/views.py
+++ funclapi/views.py
@@ -23,7 +23,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=user_id))
 
 
 
