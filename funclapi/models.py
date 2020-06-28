6b60e1d275b090f1feabacedc12cf3d9679720d7
YunJou <sheryltu880121@gmail.com>
2020-06-28 21:14:38 +0800

    062110013

--- funclapi/models.py
+++ funclapi/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-#class users(models.Model):
-#    uid = models.CharField(max_length=50, null=False)
-#    created_time = models.DateTimeField(auto_now=True)
-    
-#    def __str__(self):
- #       return self.uid
 
