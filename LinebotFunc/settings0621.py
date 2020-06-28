310c454e0702cae5c4731f1f24f3b63207b3298f
YunJou <sheryltu880121@gmail.com>
2020-06-29 00:11:52 +0800

    062110013

--- LinebotFunc/settings0621.py
+++ LinebotFunc/settings0621.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 import os
+import dj_database_url
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600,ssl_require=True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,5 +129,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
       os.path.join(BASE_DIR,'static'),]
 	  
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600,ssl_require=True)
+
