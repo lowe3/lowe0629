b8cd5c43f86fc02a4e2aab4989bbe1645afb69d7
YunJou <sheryltu880121@gmail.com>
2020-08-05 16:44:55 +0800

    08051508

--- LinebotFunc/settings.py
+++ LinebotFunc/settings.py
@@ -12,6 +12,25 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import psycopg2
 
+conn = psycopg2.connect(
+database = "d4jgiqvfjf2nde",
+user = "rdvsrzpesjtzad",
+password = "eddaf28d42616aba0fd4de92aeb8df4d1e33c6e1ae6a202da17fd1d6cd39fbaf",
+host = "ec2-34-200-101-236.compute-1.amazonaws.com",
+port = "5432")
+
+cur = conn.cursor()
+cur.execute("CREATE TABLE USER(
+ID          VARCHAR(50) PRIMARY KEY   NOT NULL,
+HEIGHT      INT                       NOT NULL,
+WEIGHT      INT                       NOT NULL,
+AGE         INT                       NOT NULL,
+GENDER      VARCHAR(10)               NOT NULL,
+BMR         INT                       NOT NULL);")
+
+conn.commit()
+conn.close()
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
