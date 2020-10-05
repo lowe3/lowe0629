ea39c8d3cc3175a0df076de7a75ec50277c7918b
YunJou <sheryltu880121@gmail.com>
2020-10-05 22:41:19 +0800

    03290601

--- funclapi/admin.py
+++ funclapi/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from funclapi.models import user, seven, wefamily
 
-# Register your models here.
+class userAdmin (admin.ModelAdmin):
+	list_display=('uid','height','weight','age','gender','bmr','tdee')
+admin.site.register(user, userAdmin)
+
+class sevenAdmin (admin.ModelAdmin):
+	list_display=('items','calories','picture')
+admin.site.register(seven, sevenAdmin)
+
+class wefamilyAdmin (admin.ModelAdmin):
+	list_display=('items','perserving','calories','protein','fat','saturatedfat','transfat','carbohydrate','sodium','sugar','picture')
+admin.site.register(seven, sevenAdmin)
--- funclapi/models.py
+++ funclapi/models.py
@@ -1,29 +1,31 @@
 from django.db import models
 
-
 class user(models.Model):
-	uid = models.CharField(max_length=50, null=False)
-	height = models.CharField(max_length=50, null=False)
-	weight = models.CharField(max_length=50, null=False)
-	age = models.CharField(max_length=50, null=False)
-	gender = models.CharField(max_length=50, null=False)
-	bmr = models.CharField(max_length=50, null=False)
+	uid = models.CharField(max_length=100, null=False)
+	height = models.CharField(max_length=100, null=False)
+	weight = models.CharField(max_length=100, null=False)
+	age = models.CharField(max_length=100, null=False)
+	gender = models.CharField(max_length=100, null=False)
+	bmr = models.CharField(max_length=100, null=False)
+	tdee = models.CharField(max_length=100, null=True)
 
 	def _str_(self):
 		return self.uid
 
 class seven(models.Model):
-	items = models.CharField(max_length=50, null=False)
-	calories = models.CharField(max_length=50, null=False)
+	items = models.CharField(max_length=100, null=False)
+	calories = models.CharField(max_length=100, null=False)
+	picture = models.CharField(max_length=250, null=True)
 
 class wefamily(models.Model):
-	items = models.CharField(max_length=50, null=False)
-	perserving = models.CharField(max_length=50)
-	calories = models.CharField(max_length=50, null=False)
-	protein = models.CharField(max_length=50)
-	fat = models.CharField(max_length=50)
-	saturatedfat = models.CharField(max_length=50)
-	transfat = models.CharField(max_length=50)
-	carbohydrate = models.CharField(max_length=50)
-	sodium = models.CharField(max_length=50)
-	sugar = models.CharField(max_length=50)
+	items = models.CharField(max_length=100, null=False)
+	perserving = models.CharField(max_length=100)
+	calories = models.CharField(max_length=100, null=False)
+	protein = models.CharField(max_length=100)
+	fat = models.CharField(max_length=100)
+	saturatedfat = models.CharField(max_length=100)
+	transfat = models.CharField(max_length=100)
+	carbohydrate = models.CharField(max_length=100)
+	sodium = models.CharField(max_length=100)
+	sugar = models.CharField(max_length=100)
+	picture = models.CharField(max_length=250, null=True)
