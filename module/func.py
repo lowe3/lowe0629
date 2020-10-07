56f37e5afb4856dfcc6fe11296903bc2b85bff46
YunJou <sheryltu880121@gmail.com>
2020-10-07 23:30:06 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 
 from linebot import LineBotApi
-from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction
+from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction, ButtonsTemplate, URITemplateAction, ConfirmTemplate, PostbackTemplateAction, TemplateSendMessage
 import random
+from funclapi.models import user, seven, wefamily
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
@@ -47,3 +48,28 @@ def sendQuickreply(event):
 		line_bot_api.reply_message(event.reply_token,message)
 	except:
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!'))
+		
+def manageForm(event, mtext, user_id):  #處理LIFF傳回的FORM資料
+    try:
+        flist = mtext[3:].split("\r")  #去除前三個「#」字元再分解字串
+        pheight = flist[0]  #取得輸入資料
+        pweight = flist[1]
+        page	= flist[2]
+        pgender = flist[3]
+		pbmr = flist[5]
+		ptdee = flist[6]
+        unit = user.objects.create(uid=user_id, height=pheight, weight=pweight, age=page, gender=pgender, bmr=pbmr, tdee=ptdee)  #寫入資料庫
+        unit.save()
+        text1 = "您的個人資料如下："
+        text1 += "\n身高：" + pheight
+        text1 += "\n體重：" + pweight
+        text1 += "\n年齡：" + page
+        text1 += "\n性別：" + pgender
+		text1 += "\n基礎代謝率：" + pbmr
+		text1 += "\n每日總消耗熱量：" + ptdee
+        message = TextSendMessage(  #顯示訂房資料
+            text = text1
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
