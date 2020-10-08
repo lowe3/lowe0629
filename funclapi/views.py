36d2b5eca31ba1daf7c5cc46de3f9953f0b43d7b
YunJou <sheryltu880121@gmail.com>
2020-10-08 12:20:13 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -36,10 +36,10 @@ def callback(request):
 		for event in events:
 			if isinstance(event, MessageEvent):
 				if isinstance(event.message, TextMessage):
-					user_id = event.source.user_id
-					if not (user.objects.filter(uid=user_id).exists()):
-						unit = user.objects.create(uid=user_id)
-						unit.save()
+					# user_id = event.source.user_id
+					# if not (user.objects.filter(uid=user_id).exists()):
+						# unit = user.objects.create(uid=user_id)
+						# unit.save()
 					mtext = event.message.text
 					if mtext == '好':
 						func.sendQuickreply(event)
