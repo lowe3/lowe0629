a2276da4296cb84e3987b0c0d221c4e1187d72d8
YunJou <sheryltu880121@gmail.com>
2020-10-13 00:45:48 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -80,9 +80,9 @@ def callback(request):
 						page = flist[2]
 						pgender = flist[3]
 						pbmr = int(''.join([x for x in flist[5] if x.isdigit()]))
-						ppbmr = round(pbmr, 2)
+						# ppbmr = round(pbmr, 2)
 						ptdee = int(''.join([x for x in flist[6] if x.isdigit()]))
-						pptdee = round(ptdee, 2)
+						# pptdee = round(ptdee, 2)
 							# user.objects.filter(uid=user_id)
 						unit = user.objects.create(uid=user_id, height=pheight, weight=pweight, age=page, gender=pgender, bmr=pbmr, tdee=ptdee)  #寫入資料庫
 						unit.save()
