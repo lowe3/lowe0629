7ec0a7b25d273dc6797c6426d4dc199e2466ccbc
YunJou <sheryltu880121@gmail.com>
2020-10-14 14:49:34 +0800

    03290601

--- funclapi/models.py
+++ funclapi/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+class users(models.Model):
+	uid = models.CharField(max_length=100, null=False, primary_key=True)
+	height = models.CharField(max_length=100, null=False)
+	weight = models.CharField(max_length=100, null=False)
+	age = models.CharField(max_length=100, null=False)
+	gender = models.CharField(max_length=100, null=False)
+	bmr = models.CharField(max_length=100, null=False)
+	tdee = models.CharField(max_length=100, null=True)
 
+	def _str_(self):
+		return self.uid
 
 class seven(models.Model):
 	items = models.CharField(max_length=100, null=False)
