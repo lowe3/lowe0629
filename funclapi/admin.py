7d389a5799be717577e8648e1eea44c227b29ee4
YunJou <sheryltu880121@gmail.com>
2020-10-14 17:15:22 +0800

    03290601

--- funclapi/admin.py
+++ funclapi/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from funclapi.models import user, seven, wefamily
-class userAdmin (admin.ModelAdmin):
-	list_display=('uid','height','weight','age','gender','bmr','tdee')
-admin.site.register(user, userAdmin)
+# from funclapi.models import user, seven, wefamily
+# class userAdmin (admin.ModelAdmin):
+	# list_display=('uid','height','weight','age','gender','bmr','tdee')
+# admin.site.register(user, userAdmin)
 
-class sevenAdmin (admin.ModelAdmin):
-	list_display=('items','calories','picture')
-admin.site.register(seven, sevenAdmin)
+# class sevenAdmin (admin.ModelAdmin):
+	# list_display=('items','calories','picture')
+# admin.site.register(seven, sevenAdmin)
 
-class wefamilyAdmin (admin.ModelAdmin):
-	list_display=('items','perserving','calories','protein','fat','saturatedfat','transfat','carbohydrate','sodium','sugar','picture')
-admin.site.register(wefamily, wefamilyAdmin)
+# class wefamilyAdmin (admin.ModelAdmin):
+	# list_display=('items','perserving','calories','protein','fat','saturatedfat','transfat','carbohydrate','sodium','sugar','picture')
+# admin.site.register(wefamily, wefamilyAdmin)
