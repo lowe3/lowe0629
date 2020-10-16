0c8688767823ec836cbc84b4eaaff8024ae76b2f
YunJou <sheryltu880121@gmail.com>
2020-10-16 15:28:34 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -80,8 +80,8 @@ def callback(request):
 						pgender = flist[3]
 						pbmr = flist[5]
 						ptdee = flist[6]
-						if user.objects.get(uid=user_id):
-							uids=user.objects.get(uid=user_id)
+						if user.objects.get(uid__exact=user_id):
+							uids=user.objects.get(uid__exact=user_id)
 							uids.update(height=pheight, weight=pweight, age=page, gender=pgender, bmr=pbmr, tdee=ptdee)  #寫入資料庫
 							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='已更新您的基本資料'))
 						else: 	
