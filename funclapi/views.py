53714831c62ee2c7b21c407a2c4c176743f7c7c5
YunJou <sheryltu880121@gmail.com>
2020-11-01 22:42:37 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -97,7 +97,7 @@ def callback(request):
 						for fitems in food.objects.filter(items__contains=mtext):
 							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='品名:' + fitems.items + '\n熱量:' + fitems.calories + '\n圖片:' + fitems.picture + '\n超商:' + fitems.convenience + '\n種類:' + fitems.kind))							
 					elif mtext[:3] == '$$$':  #處理LIFF傳回的FORM資料
-						flist = mtext[3:].split('$')
+						flist = mtext[20:].split('$')
 						edate = flist[0]  #取得輸入資料
 						# etime = flist[1]
 						# eitems = flist[4]
