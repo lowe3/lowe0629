b39fcc80976f14ff4d7941911f34df2aed8f81fc
YunJou <sheryltu880121@gmail.com>
2020-11-01 21:47:05 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from linebot import LineBotApi
 from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction, ButtonsTemplate, URITemplateAction, ConfirmTemplate, PostbackTemplateAction, TemplateSendMessage
 import random
-from funclapi.models import users, seven, wefamily, user, food, eat
+from funclapi.models import users, seven, wefamily, user, food
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 
