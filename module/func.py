7e010422b38e88d3724cc116940e55768e49f156
YunJou <sheryltu880121@gmail.com>
2020-11-02 16:07:44 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-
+import datetime
 from linebot import LineBotApi
 from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction, ButtonsTemplate, URITemplateAction, ConfirmTemplate, PostbackTemplateAction, TemplateSendMessage
 import random
@@ -20,18 +20,15 @@ def manageForm(event, mtext, user_id):
 	try:
 		flist = mtext[3:].split()
 		edatetime = flist[0]		#取得輸入資料
-		datetimee = datetime.datetime.strptime(str_p,'%Y-%m-%d %H:%M')
+		datetimee = datetime.datetime.strptime(edatetime,'%Y-%m-%d %H:%M')
 		date_time = datetimee.strftime("%m-%d-%Y, %H:%M")
 		eitems = flist[3]
-		# user_id = event.source.user_id
-		# if food.objects.filter(items=flist[3]).exists() and user.objects.filter(uid=user_id).exists():
-			# for fitems in food.objects.filter(items=flist[3]):
-				# ecalories = fitems.calories
-			# for fuser in user.objects.filter(uid=user_id)
-				# ebmr = fuser.bmr
-				# etdee = fuser.tdee
-		# unit = eat.objects.create(uid=user_id, bmr=ebmr, tdee=etdee, date=edate, time=etime, items=eitems, calories=ecalories)  #寫入資料庫
-		# unit.save()
-		line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期時間：'+edatetime+'\n產品名稱：'+eitems))
+		user_id = event.source.user_id
+		if food.objects.filter(items=flist[3]).exists() and user.objects.filter(uid=user_id).exists():
+			for fitems in food.objects.filter(items=flist[3]):
+			for fuser in user.objects.filter(uid=user_id)
+		unit = eat.objects.create(uid=user_id, bmr=fuser.bmr, tdee=fuser.tdee, datetime=datetimee, items=eitems, calories=fitems.calories)  #寫入資料庫
+		unit.save()
+		line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期時間：'+date_time+'\n產品名稱：'+eitems+'\n熱量:'+fitems.calories))
 	except:
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!'))
