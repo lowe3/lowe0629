dc695f204fe29f6747da7aecda118c70abb2b818
YunJou <sheryltu880121@gmail.com>
2020-11-02 17:55:15 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -21,7 +21,7 @@ def manageForm(event, mtext, user_id):
 		flist = mtext[3:].split()
 		edatetime = flist[0]		#取得輸入資料
 		# edt = ''.join([x for x in edatetime if x.isdigit()])     #抓取數字
-		# datetimee = datetime.strptime(edt, '%Y-%m-%d %H:%M')
+		datetimee = datetime.strptime(edatetime, '%Y-%m-%d-%H:%M')
 		# date_time = datetime.strftime(datetimee, "%m-%d-%Y, %H:%M")
 		eitems = flist[3]
 		# user_id = event.source.user_id
@@ -29,8 +29,8 @@ def manageForm(event, mtext, user_id):
 		for fitems in food.objects.filter(items=eitems):
 			content='\n熱量:'+fitems.calories
 			# for fuser in user.objects.get(uid=user_id):
-			# unit = eat.objects.create(uid=user_id, bmr=user.objects.get(uid=user_id).bmr, tdee=user.objects.get(uid=user_id).tdee, datetime=datetimee, items=eitems, calories=fitems.calories)  #寫入資料庫
-			# unit.save()
+			unit = eat.objects.create(uid=user_id, bmr=user.objects.get(uid=user_id).bmr, tdee=user.objects.get(uid=user_id).tdee, datetime=datetimee, items=eitems, calories=fitems.calories)  #寫入資料庫
+			unit.save()
 			line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期時間：'+edatetime+'\n產品名稱：'+eitems+content))
 	except:
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!'))
