062f61bd2d2ae021aa5d2060cc04ec3be3106c48
YunJou <sheryltu880121@gmail.com>
2020-11-02 16:51:47 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -26,10 +26,10 @@ def manageForm(event, mtext, user_id):
 		# user_id = event.source.user_id
 		# if food.objects.filter(items=eitems).exists():
 		for fitems in food.objects.filter(items=eitems):
-			fcalories=fitems.calories
+			content='\n熱量:'+fitems.calories
 			for fuser in user.objects.get(uid=user_id):
 		# unit = eat.objects.create(uid=user_id, bmr=fuser.bmr, tdee=fuser.tdee, datetime=datetimee, items=eitems, calories=fitems.calories)  #寫入資料庫
 		# unit.save()
-				line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期時間：'+edatetime+'\n產品名稱：'+eitems+'\n熱量:'+fcalories))
+				line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期時間：'+edatetime+'\n產品名稱：'+eitems+content))
 	except:
 		line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!'))
