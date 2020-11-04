e38710df9883e7ee5abd3281cede4c466c331afd
YunJou <sheryltu880121@gmail.com>
2020-11-04 22:03:50 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -68,7 +68,7 @@ def manageForm(event, mtext, user_id):
 		edatetime = flist[0]		#取得輸入資料
 		eitems = flist[3]
 		for fitems in food.objects.filter(items=eitems):
-			content='\n熱量:'+fitems.calories
+			content='\n熱量:'+str(fitems.calories)+'大卡'
 			unit = eat.objects.create(uid=user_id, bmr=user.objects.get(uid=user_id).bmr, tdee=user.objects.get(uid=user_id).tdee, datetime=edatetime, items=eitems, calories=fitems.calories)  #寫入資料庫
 			unit.save()
 			line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期時間：'+edatetime+'\n產品名稱：'+eitems+content))
