a7fb4cf365896eb2b03779e77f0e0fc1a445bf78
YunJou <sheryltu880121@gmail.com>
2020-11-04 17:25:10 +0800

    03290601

--- funclapi/models.py
+++ funclapi/models.py
@@ -48,22 +48,12 @@ class wefamily(models.Model):
 	def _str_(self):
 		return self.items
 		
-class food(models.Model):
-	items = models.CharField(max_length=250, null=False)
-	calories = models.CharField(max_length=100, null=False)
-	picture = models.CharField(max_length=250, null=True)
-	convenience = models.CharField(max_length=100, null=False)
-	kind = models.CharField(max_length=100, null=False)
-	
-	def _str_(self):
-		return self.items
-class eat(models.Model):
+
+
+class daily(models.Model):
 	uid = models.CharField(max_length=100, null=False)
-	bmr = models.CharField(max_length=100, null=False)
-	tdee = models.CharField(max_length=100, null=False)
-	datetime = models.CharField(max_length=100, null=False)
-	items = models.CharField(max_length=250, null=False)
-	calories = models.CharField(max_length=100, null=False)
-	
+	date = models.CharField(max_length=100, null=False)
+	calories = models.DecimalField(max_digits=10, decimal_places=2)
+		
 	def _str_(self):
 		return self.uid
