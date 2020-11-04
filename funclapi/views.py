44da8e03eee12648a91a1b825faf12747230ffc4
YunJou <sheryltu880121@gmail.com>
2020-11-04 14:19:52 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -46,7 +46,7 @@ def callback(request):
 					elif mtext == '@飯類':
 						dt = datetime.now().strftime('%Y-%m-%d')
 						for feat in eat.objects.filter(uid=user_id, datetime__contains=dt):							
-							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='我吃了什麼'+feat.calories))	
+							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='我吃了什麼'+feat.calories[1]+','+feat.calories[2]))	
 					elif mtext[:3] == '$$$':  #處理LIFF傳回的FORM資料
 						func.manageForm(event, mtext, user_id)
 					elif mtext == '餐點紀錄':
