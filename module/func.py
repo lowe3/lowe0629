2810b0bfaec36014dbe269d23e9f6d7e849d34fb
YunJou <sheryltu880121@gmail.com>
2020-11-09 22:01:26 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -70,11 +70,10 @@ def manageForm(event, mtext, user_id):
 		eitems = flist[4]
 		for fitems in food.objects.filter(items=eitems):
 			content='\n熱量:'+str(fitems.calories)+'大卡'
-			if not (eat.objects.filter(Q(uid=user_id) & Q(datetime=edate)).exists()):
-				# times = int(1)
-				# unit = eat.objects.create(uid=user_id, bmr=user.objects.get(uid=user_id).bmr, tdee=user.objects.get(uid=user_id).tdee, datetime=edate, items=eitems, calories=food.objects.filter(items=eitems).calories, total=food.objects.filter(items=eitems).calories, times=times)  #寫入資料庫
-				# unit.save()
-				line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期:'+edate+'\n時間：'+etime+'\n產品名稱：'+eitems+content))
+			# if not (eat.objects.filter(Q(uid=user_id) & Q(datetime=edate)).exists()):
+			unit = eat.objects.create(uid=user_id, bmr=user.objects.get(uid=user_id).bmr, tdee=user.objects.get(uid=user_id).tdee, datetime=edate, items=eitems, calories=food.objects.filter(items=eitems).calories, total=food.objects.filter(items=eitems).calories, times=int(1))  #寫入資料庫
+			unit.save()
+			line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期:'+edate+'\n時間：'+etime+'\n產品名稱：'+eitems+content))
 			# else:
 				# etotal = eat.objects.filter(uid=user_id, datetime=edate).lastest('total') + food.objects.filter(items=eitems)
 				# etimes = eat.objects.filter(uid=user_id, datetime=edate).lastest('times') + 1
