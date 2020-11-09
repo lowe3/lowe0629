092ca52fedbf9b0ce2e650faee5351f296d8915c
YunJou <sheryltu880121@gmail.com>
2020-11-09 23:29:37 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -77,8 +77,9 @@ def manageForm(event, mtext, user_id):
 				unit.save()
 				line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期:'+edate+'\n時間：'+etime+'\n產品名稱：'+eitems +content))
 			else:
-				etotal = eat.objects.filter(uid=user_id, datetime=edate).latest('total') + food.objects.filter(items=eitems).calories
-				etimes = eat.objects.filter(uid=user_id, datetime=edate).latest('times') + 1
+				etotal = eat.objects.filter(uid=user_id, datetime=edate).last().total + food.objects.filter(items=eitems).calories
+				etimes = eat.objects.filter(uid=user_id, datetime=edate).last().times + 1
+				# etimes = eat.objects.filter(uid=user_id, datetime=edate).latest('times') + 1
 				eunit = eat.objects.create(uid=user_id, bmr=user.objects.get(uid=user_id).bmr, tdee=user.objects.get(uid=user_id).tdee, datetime=edate, items=eitems, calories=fitems.calories, total=etotal, times=etimes)  #寫入資料庫
 				eunit.save()
 				line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您的餐點紀錄已成功輸入，輸入內容如下:'+'\n日期時間：'+edate+etime+'\n產品名稱：'+eitems+content))
