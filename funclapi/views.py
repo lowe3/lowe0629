24af9de521856616c91fa4ad3aceb610db434968
YunJou <sheryltu880121@gmail.com>
2020-11-18 16:14:56 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -92,7 +92,8 @@ def callback(request):
 							# reit_list = list(food.objects.filter(kind='飯類', calories__lte=sp).items)
 							# line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mtext))
 							#for item in food.objects.get(id=220):
-							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='推薦給您：'+food.objects.get(id=220).items))
+							items_list = food.objects.filter(kind=mtext, calories__lte=sp).items
+							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='推薦給您：'+ choice(items_list)))
 					elif mtext[:3] == '$$$':  #處理LIFF傳回的FORM資料
 						func.manageForm(event, mtext, user_id)
 					elif mtext == '餐點紀錄':
