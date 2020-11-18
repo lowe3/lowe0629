d7223907b5fa7400d9ce539d7a660b37994a46ae
YunJou <sheryltu880121@gmail.com>
2020-11-18 15:28:30 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -90,7 +90,7 @@ def callback(request):
 							surplus = etdee-eat.objects.filter(uid=user_id, datetime=dt).last().total
 							sp = surplus/2
 							# reit_list = list(food.objects.filter(kind='飯類', calories__lte=sp).items)
-							for item in food.objects.get(kind="飯類", id=220):
+							for item in food.objects.get(kind=mtext, id=220):
 								line_bot_api.reply_message(event.reply_token, TextSendMessage(text='推薦給您：'+item.items))
 					elif mtext[:3] == '$$$':  #處理LIFF傳回的FORM資料
 						func.manageForm(event, mtext, user_id)
