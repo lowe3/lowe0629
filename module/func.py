e617222d68eb92a435ecf9eea3757d0ca31c6d9b
YunJou <sheryltu880121@gmail.com>
2020-11-25 13:12:22 +0800

    03290601

--- module/func.py
+++ module/func.py
@@ -55,6 +55,7 @@ def sendQuickreply(event):  #快速選單
                 ]
             )
         )
+		
 		line_bot_api.reply_message(event.reply_token, message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
