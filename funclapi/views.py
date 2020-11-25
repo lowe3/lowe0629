f5ef344a5662aa4b64e471efe178f2ec14ce1d27
YunJou <sheryltu880121@gmail.com>
2020-11-25 12:02:53 +0800

    03290601

--- funclapi/views.py
+++ funclapi/views.py
@@ -46,7 +46,7 @@ def callback(request):
 						dt = datetime.now().strftime('%Y-%m-%d')
 						if eat.objects.filter(uid=user_id, datetime=dt).exists():
 							ttotal = str(eat.objects.filter(uid=user_id, datetime=dt).last().total)
-							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您今日已攝取熱量：'+ttotal+'大卡\n'),func.sendQuickreply(event))
+							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您今日已攝取熱量：'+ttotal+'大卡'), func.sendQuickreply(event))
 						else:
 							line_bot_api.reply_message(event.reply_token, TextSendMessage(text='您今日尚未飲食，熱量：0大卡'))
 							# func.sendQuickreply(event)
